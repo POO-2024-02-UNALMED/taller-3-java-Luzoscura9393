092879d5a687f19eaac03f37eab899a3f72e4ccf
Luzoscura9393 <anloaizag@unal.edu.co>
2024-11-19 10:51:28 -0500

    Add files via upload

--- src/main/java/taller3/televisores/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/taller3/televisores/Control.java
@@ -1,5 +1,38 @@
 package taller3.televisores;
 
 public class Control {
+    private TV tv;
 
+
+    public void turnOn() {
+        if (tv != null) {
+            tv.turnOn();
+        }
+    }
+
+    public void turnOff() {
+        if (tv != null) {
+            tv.turnOff();
+        }
+    }
+
+       public void canalUp() {
+        if (tv != null) {
+            tv.canalUp();
+        }
+    }
+
+    public void canalDown() {
+        if (tv != null) {
+            tv.canalDown();
+        }
+    }
+
+    
+    public void volumenUp() {
+        if (tv != null) {
+            tv.volumenUp();
+        }
+    }
 }
+
